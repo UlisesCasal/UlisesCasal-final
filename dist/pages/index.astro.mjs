788c31d5657f9096a9fcae4bccf9c9b8fb60a27c
Ulises Casal <casalulises@gmail.com>
2024-01-10 14:08:00 -0300

    Subo dist

---
+++ dist/pages/index.astro.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('../chunks/pages/index_IWfl7NGo.mjs');
+
+export { page };
