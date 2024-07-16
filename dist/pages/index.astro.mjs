c97499f042563c0b93270b1983360d4bf43940f6
casalulises <casalulises@gmail.com>
2024-07-16 13:06:32 +0000

    Deploy

--- dist/pages/index.astro.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('../chunks/pages/index_IWfl7NGo.mjs');
-
-export { page };
